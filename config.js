4b69d38bc34375fca698298f3990d6a1576f440f
Jerry Mesimaki <jerrym@unity3d.com>
2018-11-08 20:53:22 +0200

    Use EC2 instead of localhost

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   SIMULATION_ROUNDS: 24000,
-  SUBSCRIBE_ADDRESS: 'http://localhost:3000/subscription',
+  SUBSCRIBE_ADDRESS: 'http://34.242.205.233:3000/subscription',
   HOST: process.env.eseshost || 'localhost',
 };
